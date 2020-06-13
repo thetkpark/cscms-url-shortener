1057ba1d2efac895dfda3b5c1dc4b80ae5d767a5
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-06-13 22:59:53 +0700

    [UPDATE] landing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Landing from './Landing.js'
+import NewURL from './NewURL.js'
+import RetrieveURL from './RetrieveURL.js'
 
 class App extends Component {
 	render() {
-		return <div>Hello React</div>
+		return (
+			<div>
+				<BrowserRouter>
+					<Route exact path="/" component={Landing}></Route>
+					<Route path="/new" component={NewURL}></Route>
+					<Route exact path="/retrieve" component={RetrieveURL}></Route>
+				</BrowserRouter>
+			</div>
+		)
 	}
 }
 
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Landing.css'
+
+class Landing extends Component {
+	render() {
+		return (
+			<div id="landing">
+				<h1>This is a landing</h1>
+			</div>
+		)
+	}
+}
+
+export default Landing

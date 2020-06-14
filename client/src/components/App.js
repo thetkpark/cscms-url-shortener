c48725d0d2b165450e7161b4faa850005d825214
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-06-14 18:54:07 +0700

    [ADD] input and btn

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Landing from './Landing.js'
-import NewURL from './NewURL.js'
-import RetrieveURL from './RetrieveURL.js'
 
 class App extends Component {
 	render() {
@@ -11,8 +9,6 @@ class App extends Component {
 			<div>
 				<BrowserRouter>
 					<Route exact path="/" component={Landing}></Route>
-					<Route path="/new" component={NewURL}></Route>
-					<Route exact path="/retrieve" component={RetrieveURL}></Route>
 				</BrowserRouter>
 			</div>
 		)
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,7 +5,20 @@ class Landing extends Component {
 	render() {
 		return (
 			<div id="landing">
-				<h1>This is a landing</h1>
+				<h1>cscms url shortener</h1>
+				<div class="ui input" id="url">
+					<input type="text" placeholder="Enter your url" focus />
+				</div>
+				<div id="actionBtn">
+					<span class="btnSpace">
+						<button class="large ui blue button">Get shorten url</button>
+					</span>
+					<span class="btnSpace">
+						<button class="large ui grey button">
+							Retrieve original url
+						</button>
+					</span>
+				</div>
 			</div>
 		)
 	}

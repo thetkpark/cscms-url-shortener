8ac5a39eb06acc2e8dbc34bf62ff5ded60c7400c
Mr.SETHANANT PIPATPAKORN <62130500230@st.sit.kmutt.ac.th>
2020-06-15 11:28:22 +0700

    ADD cors and helmet

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import path from 'path'
+import cors from 'cors'
+import helmet from 'helmet'
 import { apiRouter } from './routes/apiRouter'
 import { rootRouter } from './routes/rootRoutes'
 
 export const app = express()
 
 app.use(express.static('web'))
+app.use(cors())
+app.use(helmet())
 app.use(bodyParser.json())
 app.use(apiRouter)
 app.use(rootRouter)

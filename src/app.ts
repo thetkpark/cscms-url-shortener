5909e8c02b9a4c4fd79c4de5890a0a48285ff11b
Mr.SETHANANT PIPATPAKORN <62130500230@st.sit.kmutt.ac.th>
2020-06-15 21:33:03 +0700

    REmove https

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,11 @@
 import express from 'express'
-import https from 'https'
-import fs from 'fs'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import helmet from 'helmet'
 import { apiRouter } from './routes/apiRouter'
 import { rootRouter } from './routes/rootRoutes'
-import dotenv from 'dotenv'
-dotenv.config()
 
-let httpsOptions = {}
-
-if (process.env.TLSKEYPATH && process.env.TLSCERTPATH) {
-	httpsOptions = {
-		key: fs.readFileSync(process.env.TLSKEYPATH),
-		cert: fs.readFileSync(process.env.TLSCERTPATH)
-	}
-}
-
-const app = express()
+export const app = express()
 
 app.use(express.static('client/build'))
 app.use(cors())
@@ -26,5 +13,3 @@ app.use(helmet())
 app.use(bodyParser.json())
 app.use(apiRouter)
 app.use(rootRouter)
-
-export const server = https.createServer(httpsOptions, app)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { server } from './app'
+import { app } from './app'
 
 const port = process.env.PORT || 3050
 
-server.listen(port, (): void => {
+app.listen(port, (): void => {
 	console.log(`Running on ${port}`)
 })

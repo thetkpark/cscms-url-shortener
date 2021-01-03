cfe40bb69ea645c6d4845512779beef2e47a06d6
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-01-03 15:29:37 +0700

    [ADD] Error handler in resolvers

--- src/resolvers/mutations/index.ts
+++ src/resolvers/mutations/index.ts
@@ -1,33 +1,37 @@
-import { UserInputError } from 'apollo-server-express'
+import { UserInputError, ApolloError } from 'apollo-server-express'
 import { getCosmosContainer } from '../../db/CosmosDB'
 import { generateUniqueToken } from '../../util/generateToken'
 
 const shortenUrl = async (_, { url: longurl, prefer }) => {
-	const container = await getCosmosContainer()
-	let shortenUrl: string
-	if (prefer) {
-		const existingURL = await container.items
-			.query(`SELECT * FROM url1 WHERE url1.shorturl = "${prefer}"`)
-			.fetchAll()
-		if (existingURL.resources.length !== 0)
-			throw new UserInputError(`'${prefer}' has been used`)
-		shortenUrl = prefer
-	} else {
-		shortenUrl = await generateUniqueToken()
-	}
+	try {
+		const container = await getCosmosContainer()
+		let shortenUrl: string
+		if (prefer) {
+			const existingURL = await container.items
+				.query(`SELECT * FROM url1 WHERE url1.shorturl = "${prefer}"`)
+				.fetchAll()
+			if (existingURL.resources.length !== 0)
+				throw new UserInputError(`'${prefer}' has been used`)
+			shortenUrl = prefer
+		} else {
+			shortenUrl = await generateUniqueToken()
+		}
 
-	const url = {
-		longUrl: longurl,
-		shortUrl: shortenUrl,
-		visit: 0
-	}
+		const url = {
+			longUrl: longurl,
+			shortUrl: shortenUrl,
+			visit: 0
+		}
 
-	await container.items.create({
-		longurl,
-		shorturl: shortenUrl,
-		visit: 0
-	})
-	return url
+		await container.items.create({
+			longurl,
+			shorturl: shortenUrl,
+			visit: 0
+		})
+		return url
+	} catch (error) {
+		throw new ApolloError(error)
+	}
 }
 
 export default { shortenUrl }
--- src/resolvers/queries/index.ts
+++ src/resolvers/queries/index.ts
@@ -1,17 +1,23 @@
+import { User } from '@azure/cosmos'
+import { UserInputError, ApolloError } from 'apollo-server-express'
 import { getCosmosContainer } from '../../db/CosmosDB'
 
 const longUrlQuery = async (_, { shortenPath }) => {
-	const container = await getCosmosContainer()
-	const urls = await container.items
-		.query(`SELECT * FROM url1 WHERE url1.shorturl = "${shortenPath}"`)
-		.fetchAll()
-
-	const url = {
-		longUrl: urls.resources[0].longurl,
-		shortUrl: urls.resources[0].shorturl,
-		visit: urls.resources[0].visit ? urls.resources[0].visit : 0
+	try {
+		const container = await getCosmosContainer()
+		const urls = await container.items
+			.query(`SELECT * FROM url1 WHERE url1.shorturl = "${shortenPath}"`)
+			.fetchAll()
+		if (urls.resources.length === 0) throw new UserInputError('URL not found')
+		const url = {
+			longUrl: urls.resources[0].longurl,
+			shortUrl: urls.resources[0].shorturl,
+			visit: urls.resources[0].visit ? urls.resources[0].visit : 0
+		}
+		return url
+	} catch (error) {
+		throw new ApolloError(error)
 	}
-	return url
 }
 
 export default { getLongUrl: longUrlQuery }

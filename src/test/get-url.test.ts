14248a323d061d663c59920083e6cb789a6e9a35
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-13 23:26:15 +0700

    ADD get URL test

--- src/test/get-url.test.ts
+++ src/test/get-url.test.ts
@@ -14,7 +14,26 @@ describe('Get and access the original URL from shorten URL', () => {
 			visit: 0
 		})
 	}, 10000)
+
 	it('return 400 if invalid shorturl is suppiled', async () => {
 		await request(app).get('/api/originalUrl').send().expect(400)
+	})
+
+	it('return 404 if not found', async () => {
+		await request(app).get('/api/originalUrl?url=someUrl').send().expect(404)
+	})
+
+	it('can get original url', async () => {
+		const res = await request(app)
+			.get(`/api/originalUrl?url=${preferedUrl}`)
+			.send()
+			.expect(200)
+		expect(res.body.url).toEqual(originalUrl)
+		expect(res.body.shortenUrl).toEqual(preferedUrl)
+	})
+
+	it('can redirect use to original url', async () => {
+		const res = await request(app).get(`/${preferedUrl}`).send().expect(302)
+		expect(res.headers.location).toEqual(originalUrl)
 	})
 })

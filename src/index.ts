cfff4b310eac7335d49e27db67b8ee4374a4084f
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-14 10:07:38 +0700

    ADD env check before listen

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { app } from './app'
 
 const port = process.env.PORT || 3050
 
+if (!process.env.COSMOSDB_ENDPOINT) throw new Error('COSMOSDB_ENDPOINT env is required')
+if (!process.env.COSMOSDB_KEY) throw new Error('COSMOSDB_KEY env is required')
+if (!process.env.ENTRYPOINT) throw new Error('ENTRYPOINT env is required')
+
 app.listen(port, (): void => {
 	console.log(`Running on ${port}`)
 })

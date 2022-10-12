718a1fc686db07c04db7f6735f7d7502c856e691
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-12 18:29:15 +0700

    add: working serving static

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono"
+import { serveStatic } from "hono/serve-static.module"
+
 const app = new Hono()
 
-app.get("/", (c) => c.text("Hello! Hono!"))
+// app.get("/", (c) => c.text("Hello! Hono!"))
+
+app.get("*", serveStatic({ root: "./" }))
 
 export default app

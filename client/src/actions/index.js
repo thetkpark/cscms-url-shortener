ddd928e1605cd3f4fe0888e8c49d79948b8e1555
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-13 22:04:37 +0700

    remove unsued func

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,29 +12,10 @@ export const createShorten = (url, slug) => async (dispatch) => {
 		response.answer = host + res.data.token
 		response.success = true
 	} catch (error) {
-		console.log(error)
+		response.answer = error.data.error
 	}
 	dispatch({ type: CREATE_SHORTEN, payload: response })
 }
-
-// export const createShortenWithSlug = (url, slug) => async (dispatch) => {
-// 	let response = {
-// 		answer: null,
-// 		success: false
-// 	}
-// 	try {
-// 		const res = await axios.post("/api/newUrl", { url: url, prefer: slug })
-// 		let host = window.location.href
-// 		if (host === "http://localhost:3000/") {
-// 			host = "http://localhost:3050/"
-// 		}
-// 		response.answer = host + res.data.shortUrl
-// 		response.success = true
-// 	} catch (error) {
-// 		console.log(error)
-// 	}
-// 	dispatch({ type: CREATE_SHORTEN, payload: response })
-// }
 
 export const getOriginal = (url) => async (dispatch) => {
 	if (url.length > 6 && url.lastIndexOf("/") !== -1) {
@@ -54,7 +35,7 @@ export const getOriginal = (url) => async (dispatch) => {
 		response.answer = res.data.url
 		response.success = true
 	} catch (error) {
-		console.log(error)
+		response.answer = error.data.error
 	}
 
 	dispatch({ type: GET_ORIGINAL, payload: response })
